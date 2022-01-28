c3e6be72f7224c7e8f86cda923043afc873d83d0
saad-chahi1 <saadcode123@gmail.com>
2022-01-28 22:43:29 +0100

    add Register

---
+++ ECommerce/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;  
+        }
+        // GET: AccountController
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.UserName,
+                    Email = user.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach(IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(user);
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ECommerce/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,110 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//for Category
+var closeEditModal = function () {
+    $('#editCategoryModal').modal('hide');
+}
+var closeDeleteModal = function () {
+    $('#deleteCategoryModel').modal('hide');
+}
+var closeAddModal = function () {
+    $('#addCategoryModal').modal('hide');
+}
+
+var ConfirmDelete = function (Id) {
+
+    $("#hiddenCategoryId").val(Id);
+    $("#deleteCategoryModel").modal('show');
+
+}
+
+var DeleteCategory = function () {
+
+    var Id = $("#hiddenCategoryId").val();
+
+    $.ajax({
+
+        type: "POST",
+        url: "/Category/Delete",
+        data: { id: Id },
+        success: function (result) {
+            $("#deleteCategoryModel").modal("hide");
+            $("#row_" + Id).remove();
+        }
+    });
+}
+
+var ConfirmEdit = function (Id, Name) {
+
+    $("#hiddenCategoryId").val(Id);
+    $("#editCategoryModal").modal('show');
+    $("#categoryName").val(Name);
+}
+
+var EditCategory = function () {
+
+    var Id = $("#hiddenCategoryId").val();
+    var Name = $("#categoryName").val();
+    $.ajax({
+        type: "POST",
+        url: "/Category/Edit",
+        data: { id: Id, name: Name },
+        success: function (result) {
+            $("#editCategoryModal").modal("hide");
+            document.location.reload(true);
+        }
+    });
+}
+
+//for Product
+var closeDeleteProduct = function () {
+    $('#deleteCategoryModel').modal('hide');
+}
+
+var confirmDeleteProduct = function (Id) {
+
+    $("#hiddenProductId").val(Id);
+    $("#deleteProductModel").modal('show');
+
+}
+
+var DeleteProduct = function () {
+
+    var Id = $("#hiddenProductId").val();
+
+    $.ajax({
+
+        type: "POST",
+        url: "/Product/Delete",
+        data: { id: Id },
+        success: function (result) {
+            $("#deleteProductModel").modal("hide");
+            $("#row_" + Id).remove();
+        }
+    });
+}
+
+var ConfirmEdit = function (Id, Name) {
+
+    $("#hiddenCategoryId").val(Id);
+    $("#editCategoryModal").modal('show');
+    $("#categoryName").val(Name);
+}
+
+var EditCategory = function () {
+
+    var Id = $("#hiddenCategoryId").val();
+    var Name = $("#categoryName").val();
+    $.ajax({
+        type: "POST",
+        url: "/Category/Edit",
+        data: { id: Id, name: Name },
+        success: function (result) {
+            $("#editCategoryModal").modal("hide");
+            document.location.reload(true);
+        }
+    });
+}
